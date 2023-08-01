8a59f18192df09bcd0782dc159f9727a9ded1158
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-01 12:50:45 -0400

    Create autoload.php

---
+++ system/modules/isotope_maximum_cart_quantity/config/autoload.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Register the classes */
+ClassLoader::addClasses(array
+(
+    'Bcs\Backend\IsotopeMaximumCartQuantity'            => 'system/modules/isotope_maximum_cart_quantity/library/Bcs/Backend/IsotopeMaximumCartQuantity.php',
+));
