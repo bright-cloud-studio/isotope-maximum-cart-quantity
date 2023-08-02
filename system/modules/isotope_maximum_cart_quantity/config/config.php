5f4afdad0e76c9b08125adefa860d22a05ad1194
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-08-02 10:14:22 -0400

    Update config.php

--- system/modules/isotope_maximum_cart_quantity/config/config.php
+++ system/modules/isotope_maximum_cart_quantity/config/config.php
@@ -12,3 +12,4 @@
 /* Isotope Hooks */
 $GLOBALS['ISO_HOOKS']['addProductToCollection'][] = array('Bcs\Backend\IsotopeMaximumCartQuantity', 'checkCollectionQuantity');
 $GLOBALS['ISO_HOOKS']['updateItemInCollection'][] = array('Bcs\Backend\IsotopeMaximumCartQuantity', 'updateCollectionQuantity');
+$GLOBALS['ISO_HOOKS']['copiedCollectionItems'][] = array('Bcs\Backend\IsotopeMaximumCartQuantity', 'mergeCollections');
