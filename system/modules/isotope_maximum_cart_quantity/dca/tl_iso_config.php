43c2fb75f9a475571ecc108120c449a4a7a38b84
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-13 11:49:36 -0400

    Update tl_iso_config.php

--- system/modules/isotope_maximum_cart_quantity/dca/tl_iso_config.php
+++ system/modules/isotope_maximum_cart_quantity/dca/tl_iso_config.php
@@ -35,7 +35,7 @@
         'inputType'             => 'radio',
         'options'               => array('individual' => 'Limit individual product quantity', 'combined' => 'Limit total cart quantity'),
         'eval'                  => array('mandatory'=>true, 'tl_class'=>'w50'),
-        'default'               => 'individual',
+        'default'               => '1',
         'sql'                   => "varchar(32) NOT NULL default ''"
     );
 
