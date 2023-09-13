6a3370ba3e51190833f12d7818e93b45e3b011a2
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-13 11:40:22 -0400

    Update tl_iso_config.php

--- system/modules/isotope_maximum_cart_quantity/languages/en/tl_iso_config.php
+++ system/modules/isotope_maximum_cart_quantity/languages/en/tl_iso_config.php
@@ -14,6 +14,7 @@
 /* Fields */
 $GLOBALS['TL_LANG']['tl_iso_config']['maxCartQuantity']  = array('Maximum Cart Quantity', 'Enter the maximum quantity allowed in the cart at one time. Set to \'0\' to disable quantity limiting');
 $GLOBALS['TL_LANG']['tl_iso_config']['maxCartMessage']   = array('Maximum Cart Message', 'Enter the warning message that is shown when the quantity is hit');
+$GLOBALS['TL_LANG']['tl_iso_config']['typeOfLimit']      = array('Type of Limiting', 'Select if you want to limit the total of each product or the combined cart total');
 
 /* Legends */
 $GLOBALS['TL_LANG']['tl_iso_config']['maximum_cart_quantity_legend'] = 'Maximum Cart Quantity Settings';
