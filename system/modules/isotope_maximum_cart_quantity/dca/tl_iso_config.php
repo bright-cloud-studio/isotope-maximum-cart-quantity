eeda9246b82a3631fa30854706234ebeab2ba09c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-13 11:07:19 -0400

    Create tl_iso_config.php

---
+++ system/modules/isotope_maximum_cart_quantity/dca/tl_iso_config.php
@@ -0,0 +1,32 @@
+<?php
+
+  /**
+  * Bright Cloud Studio's Isotope Maximum Cart Quantity
+  *
+  * Copyright (C) 2023 Bright Cloud Studio
+  *
+  * @package    bright-cloud-studio/isotope-maximum-cart-quantity
+  * @link       https://www.brightcloudstudio.com/
+  * @license    http://opensource.org/licenses/lgpl-3.0.html
+  **/
+  
+  /* Extend the tl_user palettes */
+  $GLOBALS['TL_DCA']['tl_iso_config']['palettes']['default'] = str_replace('{analytics_legend}', '{maximum_cart_quantity_legend},maxCartQuantity,maxCartMessage;{analytics_legend}', $GLOBALS['TL_DCA']['tl_iso_config']['palettes']['default']);
+  
+  $GLOBALS['TL_DCA']['tl_iso_config']['fields']['maxCartQuantity'] = array
+  (
+  	'label'                 => &$GLOBALS['TL_LANG']['tl_iso_config']['maxCartQuantity'],
+  	'inputType'             => 'text',
+    'eval'                  => array('mandatory'=>false, 'maxlength'=>10, 'tl_class'=>'w50'),
+    'sql'                   => "varchar(10) NOT NULL default ''",
+  );
+
+  $GLOBALS['TL_DCA']['tl_iso_config']['fields']['maxCartMessage'] = array
+  (
+  	'label'                 => &$GLOBALS['TL_LANG']['tl_iso_config']['maxCartMessage'],
+  	'inputType'             => 'text',
+    'eval'                  => array('maxlength'=>255, 'tl_class'=>'w50'),
+    'sql'                   => "varchar(255) NOT NULL default ''",
+  );
+
+?>
