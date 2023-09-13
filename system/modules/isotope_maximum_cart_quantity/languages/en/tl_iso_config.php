96d3e8e87a980907db6a3733416b5993c5960677
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-13 11:13:16 -0400

    Update and rename tl_iso_product.php to tl_iso_config.php

---
+++ system/modules/isotope_maximum_cart_quantity/languages/en/tl_iso_config.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Isotope Maximum Cart Quantity
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/isotope-maximum-cart-quantity
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_iso_config']['maxCartQuantity']  = array('Maximum Cart Quantity', 'Enter the maximum quantity allowed in the cart at one time. Set to \'0\' to disable quantity limiting');
+$GLOBALS['TL_LANG']['tl_iso_config']['maxCartMessage']   = array('Maximum Cart Message', 'Enter the warning message that is shown when the quantity is hit');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_iso_config']['maximum_cart_quantity_legend'] = 'Maximum Cart Quantity Settings';
